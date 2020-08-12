37b570da635abbb44818a3530bb9d70ac7615697
Dylan Tsou <51984498+dylantsouy@users.noreply.github.com>
2020-08-12 18:58:56 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,161 @@
+{
+  let nav = document.querySelector("#nav");
+  let logo = document.querySelector("#logo");
+  let menuShow = document.querySelector("#menuShow");
+  let menuHidden = document.querySelector("#menuHidden");
+  let navlink = nav.querySelectorAll("a");
+  let slideBtn = document.querySelector("#slideBtn");
+  let h1 = document.querySelectorAll("h1");
+  let experience = document
+    .querySelector("#experience")
+    .querySelector(".section");
+  let aboutL = document.querySelector("#about").querySelector(".pic");
+  let aboutR = document.querySelector("#about").querySelector(".txt");
+  let skillsTxt = document.querySelector("#skills").querySelectorAll(".txt");
+  let worksPic = document.querySelector("#works").querySelectorAll(".pic");
+  let worksTxt = document.querySelector("#works").querySelectorAll(".txt");
+
+  // --- 展開菜單欄 ---
+  menuShow.addEventListener("click", evt => {
+    //   nav.style.setProperty("display", "flex");
+    evt.preventDefault();
+    nav.style.setProperty("visibility", "visible");
+    nav.style.setProperty("max-height", "300px");
+    menuShow.style.setProperty("display", "none");
+    menuHidden.style.setProperty("display", "block");
+  });
+  // --- 關閉菜單欄 ---
+  menuHidden.addEventListener("click", evt => {
+    evt.preventDefault();
+    nav.style.setProperty("visibility", "hidden");
+    nav.style.setProperty("max-height", "0px");
+    menuShow.style.setProperty("display", "block");
+    menuHidden.style.setProperty("display", "none");
+  });
+  // --- 點擊菜單欄連結 ---
+  navlink.forEach(element => {
+    element.addEventListener("click", evt => {
+      evt.preventDefault();
+      nav.style.setProperty("visibility", "hidden");
+      nav.style.setProperty("max-height", "0px");
+      menuShow.style.setProperty("display", "block");
+      menuHidden.style.setProperty("display", "none");
+    });
+  });
+  // --- 點選logo --
+  logo.addEventListener("click", evt => {
+    evt.preventDefault();
+    scrollTo(0, 600, ".home");
+  });
+  // --- 點選slideBtn ---
+  slideBtn.addEventListener("click", evt => {
+    evt.preventDefault();
+    scrollTo(0, 600, ".experience");
+  });
+  // --- 點選experience ---
+  navlink[0].addEventListener("click", evt => {
+    evt.preventDefault();
+    scrollTo(0, 600, ".experience");
+  });
+  // --- 點選about ---
+  navlink[1].addEventListener("click", evt => {
+    evt.preventDefault();
+    scrollTo(0, 600, ".about");
+  });
+  // --- 點選skills ---
+  navlink[2].addEventListener("click", evt => {
+    evt.preventDefault();
+    scrollTo(0, 600, ".skills");
+  });
+  // --- 點選works ---
+  navlink[3].addEventListener("click", evt => {
+    evt.preventDefault();
+    scrollTo(0, 600, ".works");
+  });
+
+  window.addEventListener("scroll", () => {
+    const top1 = document.documentElement.scrollTop;
+    const top2 = document.querySelector(".experience").offsetTop;
+    const top3 = document.querySelector(".about").offsetTop;
+    const top4 = document.querySelector(".skills").offsetTop;
+    const top5 = document.querySelector(".works").offsetTop;
+    const h = window.screen.height;
+    // --- 學經歷是否顯示 ---
+    if (top1 + h / 2 > top2 && top1 + h / 2 < top3) {
+      experience.classList.add("active");
+      h1[1].classList.add("active");
+      navlink[0].classList.add("activeNav");
+    } else {
+      experience.classList.remove("active");
+      h1[1].classList.remove("active");
+      navlink[0].classList.remove("activeNav");
+    }
+    // --- 關於我是否顯示 ---
+    if (top1 + h / 2 > top3 && top1 + h / 2 < top4) {
+      aboutL.classList.add("active");
+      aboutR.classList.add("active");
+      h1[2].classList.add("active");
+      navlink[1].classList.add("activeNav");
+    } else {
+      aboutL.classList.remove("active");
+      aboutR.classList.remove("active");
+      h1[2].classList.remove("active");
+      navlink[1].classList.remove("activeNav");
+    }
+    // --- 技能樹是否顯示 ---
+    if (top1 + h / 2 > top4 && top1 + h / 2 < top5) {
+      skillsTxt.forEach(el => {
+        el.classList.add("active");
+      });
+      h1[3].classList.add("active");
+      navlink[2].classList.add("activeNav");
+    } else {
+      skillsTxt.forEach(el => {
+        el.classList.remove("active");
+      });
+      h1[3].classList.remove("active");
+      navlink[2].classList.remove("activeNav");
+    }
+    // --- 作品是否顯示 ---
+    if (top1 + h / 2 > top5) {
+      worksPic.forEach(el => {
+        el.classList.add("active");
+      });
+      worksTxt.forEach(el => {
+        el.classList.add("active");
+      });
+      h1[4].classList.add("active");
+      navlink[3].classList.add("activeNav");
+    } else {
+      worksPic.forEach(el => {
+        el.classList.remove("active");
+      });
+      worksTxt.forEach(el => {
+        el.classList.remove("active");
+      });
+      h1[4].classList.remove("active");
+      navlink[3].classList.remove("activeNav");
+    }
+  });
+
+  function scrollTo(number = 0, time, element) {
+    if (!time) {
+      document.body.scrollTop = document.documentElement.scrollTop = number;
+      return number;
+    }
+    const top1 = document.documentElement.scrollTop;
+    const top2 = document.querySelector(element).offsetTop;
+    const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
+    let spacingInex = time / spacingTime; // 计算循环的次数
+    let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
+    let everTop = (top2 - top1) / spacingInex; // 计算每次滑动的距离
+    let scrollTimer = setInterval(() => {
+      if (spacingInex > 0) {
+        spacingInex--;
+        this.scrollTo((nowTop += everTop));
+      } else {
+        clearInterval(scrollTimer); // 清除计时器
+      }
+    }, spacingTime);
+  }
+}
